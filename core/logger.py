2323f2c8e64f124f4c1fa7a6eaa44177ac3aea23
amirkb91 <a.bagheri20@imperial.ac.uk>
2022-12-12 23:18:53 +0000

    SE2 working and plot

--- core/logger.py
+++ core/logger.py
@@ -36,13 +36,14 @@ class Logger:
             elif key == "sol_beta":
                 self.sol_beta.append(value)
 
-        # save to disk and plot if required
-        self.savetodisk()
-        # if self.solidx > 1 and self.prob.cont_params["plot"]:
-        #     self.solplot()
+        if self.store_index % 1 == 0:
+            # save to disk and plot if required
+            self.savetodisk()
+            if self.prob.cont_params["plot"]:
+                self.solplot()
 
     def savetodisk(self):
-        savefile = h5py.File("continuation_sol.h5", "w")
+        savefile = h5py.File("solution.h5", "w")
         savefile["/X"] = np.asarray(self.sol_X).T
         savefile["/T"] = np.asarray(self.sol_T).T
         savefile["/Tangent"] = np.asarray(self.sol_tgt).T
@@ -54,50 +55,44 @@ class Logger:
         savefile.close()
 
     def solplot(self):
+        Energy = np.asarray(self.sol_energy)
+        T = np.asarray(self.sol_T)
+        beta = np.asarray(self.sol_beta)
         beta_xaxis = 10
+        betaplot = False
+        if self.prob.cont_params["continuation"]["method"] == "psa":
+            betaplot = True
+
         if not self.fig:
-            if self.beta.all():
-                self.fig, (self.ax1, self.ax2) = plt.subplots(1, 2, figsize=(8, 6))
+            if betaplot:
+                self.fig, (self.ax1, self.ax2) = plt.subplots(1, 2, figsize=(10, 7))
             else:
-                self.fig, (self.ax1) = plt.subplots(1, 1, figsize=(4, 6))
+                self.fig, (self.ax1) = plt.subplots(1, 1, figsize=(6, 4.5))
             # Frequency energy plot
             self.ax1.grid()
             self.ax1.set_xscale("log")
             self.ax1.set_xlabel("Energy (J)")
             self.ax1.set_ylabel("Frequency (Hz)")
             self.ax1.ticklabel_format(useOffset=False, axis="y")
-            # self.ax1.set_xlim(1e-10, 1e4)
-            # self.ax1.set_ylim(
-            #     self.prob.cont_params["continuation"]["fmin"],
-            #     self.prob.cont_params["continuation"]["fmax"],
-            # )
-            (self.line1,) = self.ax1.plot(
-                self.outputs["energy"], 1 / self.solT, marker=".", fillstyle="none"
-            )
+            # self.ax1.set_xlim(1e-6, 1e4)
+            # self.ax1.set_ylim(self.prob.cont_params["continuation"]["fmin"],
+            #                   self.prob.cont_params["continuation"]["fmax"])
+            (self.line1,) = self.ax1.plot(Energy, 1 / T, marker=".", fillstyle="none")
             # beta plot
-            if self.beta.all():
+            if betaplot:
                 self.ax2.grid()
                 self.ax2.set_xlabel("Continuation Step")
                 self.ax2.set_ylabel("beta (deg)")
                 self.ax2.set_xlim(1, beta_xaxis)
-                self.ax2.set_ylim(0, 180)
-                (self.line2,) = self.ax2.plot(
-                    range(len(self.beta)),
-                    self.beta,
-                    marker=".",
-                    fillstyle="none",
-                    color="red",
-                )
-
-            plt.tight_layout()
-            plt.pause(0.5)
-
+                self.ax2.set_ylim(-5, 185)
+                (self.line2,) = self.ax2.plot(range(1, len(beta) + 1), beta, marker=".", fillstyle="none", color="red")
+            plt.pause(0.01)
         else:
-            self.line1.set_data(self.outputs["energy"], 1 / self.solT)
+            self.line1.set_data(Energy, 1 / T)
             self.ax1.relim()
             self.ax1.autoscale()
-            if self.beta.all():
-                self.line2.set_data(range(len(self.beta)), self.beta)
-                self.ax2.set_xlim(1, beta_xaxis * np.ceil(len(self.beta) / beta_xaxis))
-            self.fig.canvas.draw()
-            self.fig.canvas.flush_events()
+            if betaplot:
+                self.line2.set_data(range(1, len(beta) + 1), beta)
+                self.ax2.set_xlim(1, beta_xaxis * np.ceil(len(beta) / beta_xaxis))
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
