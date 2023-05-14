c1f7168092ca4447715f0396aed5701c6b646890
amirkb91 <a.bagheri20@imperial.ac.uk>
2023-05-14 08:40:31 +0100

    changes propagated to cubic spring

--- examples/cubic_spring/plot_timesol.py
+++ examples/cubic_spring/plot_timesol.py
@@ -2,6 +2,8 @@ import h5py
 import sys
 import json
 import numpy as np
+from scipy.integrate import odeint
+from examples.cubic_spring.cubic_spring import Cubic_Spring
 import matplotlib.pyplot as plt
 
 # inputs
@@ -12,8 +14,8 @@ file = sys.argv[-1]
 if not file.endswith(".h5"):
     file += ".h5"
 data = h5py.File(str(file), "r")
-pos = data["/Config/POSE_time"][:, :, solno]
-vel = data["/Config/VELOCITY_time"][:, :, solno]
+pos = data["/Config/POSE"][:, solno]
+vel = data["/Config/VELOCITY"][:, solno]
 T = data["/T"][solno]
 
 # check parameters for time simulation data
@@ -35,18 +37,24 @@ elif method == "multiple":
         partition_timestamp = ipart * T_part
         print(f"{partition_timestamp:.3f}")
 
+# run sim
+X = np.concatenate([pos, vel])
+timesol = np.array(odeint(Cubic_Spring.system_ode, X, t, rtol=1e-8, tfirst=True))
+pose_time = timesol[:, :2]
+vel_time = timesol[:, 2:]
+
 # figure properties
 f, (a1, a2, a3) = plt.subplots(1, 3, figsize=(10, 4))
 f.suptitle(f"Frequency = {1 / T:.3f} Hz")
 a1.set(xlabel="Time (s)", ylabel="Position (m)")
-a1.plot(t, pos[0, :], '-', label="DoF 1")
-a1.plot(t, pos[1, :], '--', label="DoF 2")
+a1.plot(t, pose_time[:, 0], '-', label="DoF 1")
+a1.plot(t, pose_time[:, 1], '--', label="DoF 2")
 a1.legend()
 a2.set(xlabel="Position DoF 1 (m)", ylabel="Position DoF 2 (m)")
-a2.plot(pos[0, :], pos[1, :], '-')
+a2.plot(pose_time[:, 0], pose_time[:, 1], '-')
 a3.set(xlabel="Time (s)", ylabel="Velocity (m/s)")
-a3.plot(t, vel[0, :], '-', label="DoF 1")
-a3.plot(t, vel[1, :], '--', label="DoF 2")
+a3.plot(t, vel_time[:, 0], '-', label="DoF 1")
+a3.plot(t, vel_time[:, 1], '--', label="DoF 2")
 a3.legend()
 
 plt.tight_layout()
--- examples/cubic_spring/runscript.py
+++ examples/cubic_spring/runscript.py
@@ -3,20 +3,20 @@ from core.logger import Logger
 from core.solver.continuation import ConX
 from core.startingpoint import StartingPoint
 
-from duffing import Duffing
+from cubic_spring import Cubic_Spring
 
 # Problem
 prob = Prob()
 prob.read_contparams("contparameters.json")
-prob.add_icfunction(Duffing.eigen_solve)
-prob.add_doffunction(Duffing.get_fe_data)
+prob.add_icfunction(Cubic_Spring.eigen_solve)
+prob.add_doffunction(Cubic_Spring.get_fe_data)
 if prob.cont_params["shooting"]["method"] == "single":
-    prob.add_zerofunction(Duffing.time_solve)
+    prob.add_zerofunction(Cubic_Spring.time_solve)
     prob.zerofunction_firstpoint = prob.zerofunction
 elif prob.cont_params["shooting"]["method"] == "multiple":
-    prob.add_zerofunction(Duffing.time_solve_multiple)
-    prob.add_zerofunction_firstpoint(Duffing.time_solve)
-    prob.add_partitionfunction(Duffing.partition_singleshooting_solution)
+    prob.add_zerofunction(Cubic_Spring.time_solve_multiple)
+    prob.add_zerofunction_firstpoint(Cubic_Spring.time_solve)
+    prob.add_partitionfunction(Cubic_Spring.partition_singleshooting_solution)
 
 # Continuation starting point
 start = StartingPoint(prob)
