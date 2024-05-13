5d8109509e5823cf3aa0b781deeea372f9344a66
amirkb91 <a.bagheri20@imperial.ac.uk>
2024-05-13 22:03:43 +0100

    Lie Group Periodicity - INC working in 3D

--- examples/beam_cpp/Frame.py
+++ examples/beam_cpp/Frame.py
@@ -7,12 +7,13 @@ class Frame:
     def relative_frame(cls, n_dim, frame_a, frame_b):
         # f = frame_a^-1 o frame_b
         if n_dim == 2:
-            q = UnitQuaternion.relative_rotation(n_dim, frame_a[:2], frame_b[:2])
-            x = UnitQuaternion.rotateT_vec(n_dim, frame_a[:2], frame_b[2:] - frame_a[2:])
+            frame_a_q, frame_a_x = frame_a[:2], frame_a[2:]
+            frame_b_q, frame_b_x = frame_b[:2], frame_b[2:]
         elif n_dim == 3:
-            q = UnitQuaternion.relative_rotation(n_dim, frame_a[:4], frame_b[:4])
-            x = UnitQuaternion.rotateT_vec(n_dim, frame_a[:4], frame_b[4:] - frame_a[4:])
-
+            frame_a_q, frame_a_x = frame_a[:4], frame_a[4:]
+            frame_b_q, frame_b_x = frame_b[:4], frame_b[4:]
+        q = UnitQuaternion.relative_rotation(n_dim, frame_a_q, frame_b_q)
+        x = UnitQuaternion.rotateT_vec(n_dim, frame_a_q, frame_b_x - frame_a_x)
         f = np.concatenate([q, x])
         return f
 
@@ -20,11 +21,11 @@ class Frame:
     def get_parameters_from_frame(cls, n_dim, frame):
         if n_dim == 2:
             p = np.zeros((3, ))
-            p[2] = 2.0 * frame[1]
+            p[2] = 2.0 * np.sign(frame[0]) * frame[1]
             p[:2] = frame[0] * frame[2:] + np.array([frame[3], -frame[2]]) * frame[1]
         elif n_dim == 3:
             p = np.zeros((6, ))
-            p[3:] = 2.0 * frame[1:3]
+            p[3:] = 2.0 * np.sign(frame[0]) * frame[1:4]
             Tm1T = frame[0] * np.eye(3) - tilde(frame[1:4])
             p[:3] = np.matmul(Tm1T, frame[4:])
         return p
