5f136c0f5bdb233890ef6055a2e6af23d6a7f548
amirkb91 <a.bagheri20@imperial.ac.uk>
2025-08-01 22:23:16 +0100

    timesim modal or physical for beam springs

--- examples/beam_spring/timesim_branch.py
+++ examples/beam_spring/timesim_branch.py
@@ -12,6 +12,10 @@ run_bif = input("Compute bifurcation functions (y/n)? ")
 if run_bif not in ("y", "n"):
     raise Exception("Input not valid")
 
+store_physical = input("Store physical displacement at tip (y) or modal coordinates (n)? ")
+if store_physical not in ("y", "n"):
+    raise Exception("Input not valid")
+
 # read solution file
 file = sys.argv[1]
 inplace = sys.argv[-1]
@@ -41,10 +45,23 @@ else:
 
 n_solpoints = len(T)
 nsteps = par["shooting"]["single"]["nsteps_per_period"]
-pose_time = np.zeros([np.shape(pose)[0], nsteps + 1, n_solpoints])
-vel_time = np.zeros([np.shape(vel)[0], nsteps + 1, n_solpoints])
-acc_time = np.zeros([np.shape(vel)[0], nsteps + 1, n_solpoints])
+
+# Determine storage dimensions based on choice
+if store_physical == "y":
+    # For physical displacement: 1 DOF (tip displacement)
+    n_dof_store = 1
+    storage_label = "Physical Tip Displacement"
+else:
+    # For modal coordinates: 2 DOF (modal coordinates)
+    n_dof_store = np.shape(pose)[0]
+    storage_label = "Modal Coordinates"
+
+pose_time = np.zeros([n_dof_store, nsteps + 1, n_solpoints])
+vel_time = np.zeros([n_dof_store, nsteps + 1, n_solpoints])
+acc_time = np.zeros([n_dof_store, nsteps + 1, n_solpoints])
 time = np.zeros([n_solpoints, nsteps + 1])
+
+print(f"Storage configuration: {storage_label}")
 
 # run sims
 Beam_Spring.forcing_parameters(par)
@@ -62,10 +79,27 @@ with alive_bar(n_solpoints) as bar:
         [_, J, pose_time_series, vel_time_series, acc_time_series, _, _] = Beam_Spring.time_solve(
             1.0, F[i], T[i], X, pose[:, i], par, fulltime=True
         )
-        # Store the time series data
-        pose_time[:, :, i] = pose_time_series.T
-        vel_time[:, :, i] = vel_time_series.T
-        acc_time[:, :, i] = acc_time_series.T
+        
+        if store_physical == "y":
+            # Convert modal coordinates to physical displacement at tip
+            phi_L = Beam_Spring.phi_L  # Mode shapes at tip location
+            
+            # Transform modal coordinates to physical displacement: u_tip = phi_L @ q
+            physical_pose = phi_L @ pose_time_series.T  # Shape: (1, nsteps+1)
+            physical_vel = phi_L @ vel_time_series.T    # Shape: (1, nsteps+1)
+            
+            # For acceleration: a_tip = phi_L @ q_ddot
+            physical_acc = phi_L @ acc_time_series.T    # Shape: (1, nsteps+1)
+            
+            # Store physical displacement data
+            pose_time[:, :, i] = physical_pose
+            vel_time[:, :, i] = physical_vel
+            acc_time[:, :, i] = physical_acc
+        else:
+            # Store modal coordinates directly
+            pose_time[:, :, i] = pose_time_series.T
+            vel_time[:, :, i] = vel_time_series.T
+            acc_time[:, :, i] = acc_time_series.T
 
         if run_bif == "y":
             M = J[:, :-1] + np.eye(4)
@@ -80,6 +114,16 @@ with alive_bar(n_solpoints) as bar:
 
 # write to file
 time_data = h5py.File(new_file, "a")
+
+# Add metadata about coordinate system
+if "/Config_Time/Coordinate_System" in time_data.keys():
+    del time_data["/Config_Time/Coordinate_System"]
+if store_physical == "y":
+    coordinate_info = "Physical displacement at beam tip"
+else:
+    coordinate_info = "Modal coordinates"
+time_data["/Config_Time/Coordinate_System"] = coordinate_info
+
 if "/Config_Time/POSE" in time_data.keys():
     del time_data["/Config_Time/POSE"]
 if "/Config_Time/VELOCITY" in time_data.keys():
@@ -109,3 +153,6 @@ if run_bif == "y":
     time_data["/Bifurcation/Flip"] = Flip
     time_data["/Bifurcation/Neimark_Sacker"] = Neimark_Sacker
 time_data.close()
+
+print(f"\nCompleted! Data stored in: {new_file}")
+print("="*60)
